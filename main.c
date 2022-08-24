9ce0620a50897ad57d019e00767c78d692884b3a
Giorno <mapleaflame@gmail.com>
2022-08-24 05:57:12 -0400

    Add function child_crawling() to crawling the web

--- main.c
+++ main.c
@@ -93,3 +93,245 @@ void parent_waiting(int process_total)
     }
 }
 
+void child_crawling()
+{
+    usleep(10000);      //防止 child 先跑
+	printf("create sub process id: %d, parent id: %d\n", getpid(), getppid());
+
+    char list_file[MAX_NAME_SIZE];
+    sprintf(list_file, "%s%s", folder, LIST_NAME);
+
+    char record_file[MAX_NAME_SIZE];
+    sprintf(record_file, "%sPID-%d%s", folder, getpid(), ".txt");
+
+    int attempt = 0;
+
+    while(attempt <= 10)
+    {
+        FILE* fptr = fopen(list_file, "r+");
+        if (!fptr)
+        {
+            printf("---------------\nfopen error !\n---------------\n");
+            exit(1);
+        }
+    
+        if (flock(fileno(fptr), LOCK_EX))
+        {
+            printf("---------------\nflock error !\n---------------\n");
+            exit(1);
+        }
+
+        struct stat fstat;
+        if (stat(list_file, &fstat))
+            printf("%s\n", strerror(errno));
+
+        int fsize = fstat.st_size;
+        if (fsize <= 0)
+        {
+            printf("---------------\nlist file empty !\n---------------\n");
+            exit(1);
+        }
+    
+        char* list_buf = malloc(fsize);
+        if (!list_buf)
+            printf("---------------\nmalloc fail !\n---------------\n");
+    
+        if (fread(list_buf, fsize, 1, fptr) != 1)
+            printf("---------------\nfread error !\n---------------\n");
+
+        char* url_loc = strstr(list_buf, "\n   http");
+        if (url_loc)
+        {
+            int seek_loc = url_loc - list_buf + sizeof(char);
+#ifdef DEBUG
+            printf("---------------\nseek_loc: %d\n", seek_loc);
+#endif
+            fseek(fptr, seek_loc, SEEK_SET);
+            fprintf(fptr, "%c", STATUS_CRAWLING);
+            flock(fileno(fptr), LOCK_UN);
+            printf("---------------\nLOCK_UN OK\n");
+            fclose(fptr);
+
+            //將 url_list 中的網址取出
+
+            url_loc += sizeof(padding);
+            char* url_end = strchr(url_loc, '\n');
+            if (url_end == NULL)
+            {
+                printf("---------------\nURL capture error !\n");
+                exit(1);
+            }
+
+
+            printf("---------------\nurl_end: %p\n", url_end);
+            int url_size = url_end - url_loc;
+            printf("---------------\nurl_size: %d\n", url_size);
+            char url[url_size + 1];
+
+            strncpy(url, url_loc, url_size);
+            url[url_size] = '\0';
+            free(list_buf);
+
+            //將 url_list 中的網址抓下
+            printf("抓取 URL: %s\n", url);
+
+            char host[MAX_URL_SIZE];    //統一 size，不然 define 好雜
+            char path[MAX_URL_SIZE];
+            get_host_n_path(url, host, MAX_URL_SIZE, path, MAX_URL_SIZE);
+            printf("----------\nhost: %s\n", host);
+            printf("----------\npath: %s\n", path);
+
+            ssl_info* SI;
+
+            if (ssl_connect(url, SI, host) == ERR_SSL_CONNCET)
+            {
+                connect_fail_handle(list_file, seek_loc);
+                continue;
+            }
+
+            char url_move[MAX_URL_SIZE] = {0};    //遇到網址轉移時
+
+            char* content = malloc(MAX_CONTENT_SIZE);
+            int content_size = 0;
+            
+            crawler(SI->ssl, host, path, record_file, url_move, 
+                                 SI->server, content, &content_size);
+            if (url_move[0])
+            {
+                ssl_disconnect(SI, url);
+                get_host_n_path(url_move, host, MAX_URL_SIZE, path, MAX_URL_SIZE);
+#ifdef DEBUG
+                printf("----------\nNEW host: %s\n", host);
+                printf("----------\nNEW path: %s\n", path);
+#endif
+                if (ssl_connect(url, SI, host) == ERR_SSL_CONNCET)
+                {
+                    connect_fail_handle(list_file, seek_loc);
+                    continue;
+                }
+                crawler(SI->ssl, host, path, record_file, url_move, SI->server, content, &content_size);
+            }
+
+            char* link_buf = calloc(BUF_SIZE, sizeof(char));
+            printf("link_buf: %p\n---------------\n", link_buf);
+
+            if (!link_buf)
+                printf("---------------\nmalloc fail !\n---------------\n");
+       
+            int link_buf_size = 0;
+            search_link(content, &content_size, link_buf, &link_buf_size);
+
+            printf("link_buf:\n%s\n", link_buf);
+            printf("---------------\nlink_buf_size: %d\n---------------\n", link_buf_size);
+#ifdef DEBUG
+            if (link_buf_size > 10000)
+                sleep(3);
+#endif
+
+            fptr = fopen(list_file, "r+");
+            if (!fptr)
+            {
+                printf("---------------\nfopen error !\n---------------\n");
+                exit(1);
+            }
+
+            if (flock(fileno(fptr), LOCK_EX))
+            {
+                printf("---------------\nflock error !\n---------------\n");
+                exit(1);
+            }
+
+            struct stat fstat;
+            stat(list_file, &fstat);
+            if (stat(list_file, &fstat))
+                printf("%s\n", strerror(errno));
+            
+            int fsize = fstat.st_size;
+            if (fsize <= 0)
+            {
+                printf("---------------\nlist file empty !\n---------------\n");
+                return;
+            }
+
+            list_buf = malloc(fsize + 1);
+            if (!list_buf)
+            {
+                printf("---------------\nmalloc fail !\n---------------\n");
+                return;
+            }
+
+            if (fread(list_buf, fsize, 1, fptr) != 1)
+            {
+                printf("---------------\nfread error !\n---------------\n");
+                return;
+            }
+            list_buf[fsize] = '\0';
+
+            //將狀態改成完成
+            fseek(fptr, seek_loc, SEEK_SET);
+            fprintf(fptr, "%c", STATUS_FINISH);
+            fseek(fptr, 0, SEEK_END);
+
+            if (link_buf[0])     //將網址寫回 list_file
+            {
+                char* curr_ptr = link_buf;
+                printf("curr_ptr: %p\n---------------\n", curr_ptr);
+
+                while (curr_ptr < link_buf + link_buf_size)
+                {
+                    char* link_end = strchr(curr_ptr, '\n');
+                    if (link_end == NULL)
+                        break;
+                    int link_size = link_end - curr_ptr;
+#ifdef DEBUG
+                    printf("link_end: %p\n---------------\n", link_end);
+                    printf("link_size: %d\n---------------\n", link_size);
+#endif
+                    if (link_size <= 0)
+                    {
+                        printf("link_size error!\n---------------\n");
+                        exit(1);
+                    }
+
+                    char link[link_size + 1];
+                    strncpy(link, curr_ptr, link_size);
+                    link[link_size] = '\0';
+
+                    char* repeat_link = strstr(list_buf, link);
+                    if (repeat_link == NULL)
+                    {
+                        fwrite(padding, sizeof(padding), 1, fptr);
+                        fwrite(link, link_size, 1, fptr);
+                    }
+                    curr_ptr = link_end + 1;
+                }
+            }
+
+            flock(fileno(fptr), LOCK_UN);
+            fclose(fptr);
+
+            if (url_move[0])
+                ssl_disconnect(SI, url_move);
+            else
+                ssl_disconnect(SI, url);
+        }
+        else
+        {
+            flock(fileno(fptr), LOCK_UN);
+            fclose(fptr);
+
+            url_loc = strstr(list_buf, "\n~  http");
+            free(list_buf);
+
+            if (url_loc)
+            {
+                sleep(1);
+                attempt++;
+            }
+            else
+                exit(0);        //子行程結束
+        }
+    }
+    exit(1);
+}
+
