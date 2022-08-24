c6fdbe91f216290007fb7e463ecdb9296e779924
Giorno <mapleaflame@gmail.com>
2022-08-24 05:26:36 -0400

    add function create_socket() for standard TCP socket connection

--- ssl.c
+++ ssl.c
@@ -9,6 +9,9 @@
 
 int ssl_connect(char* url, ssl_info* SI, char* host)
 {
+    //同網域的話 ssl 連線只須建立一次
+    //再抓網址時若是 http 需判斷 是否同網域
+    //將 host_name 變成全域變數，讓程式可以更簡化
 
     if (!SI)
         return ERR_SSL_INFO;
@@ -75,7 +78,7 @@ int ssl_connect(char* url, ssl_info* SI, char* host)
     SI->server = create_socket(url, host);
     if (SI->server == -1)
         return ERR_SSL_CONNCET;
-    if (SI->server)
+    else if (SI->server)
         printf("Successfully made the TCP connection to: %s\n", url);
 
   /* ---------------------------------------------------------- *
@@ -112,8 +115,93 @@ int ssl_connect(char* url, ssl_info* SI, char* host)
         return ERR_SSL_CONNCET;
     }
     else
+#ifdef DEBUG
         printf("Successfully enabled SSL/TLS session to: %s\n", url);
-
+#endif
     return SUCCESS;
 }
+
+
+/* ---------------------------------------------------------- *
+ * create_socket() creates the socket & TCP-connect to server *
+ * ---------------------------------------------------------- */
+int create_socket(char* url, char* host_url)
+{
+    int sockfd;
+    char portnum[6] = "443";
+    char proto[6] = "";
+    char *tmp_ptr = NULL;
+    int port;
+    struct hostent* host;
+    struct sockaddr_in dest_addr;
+
+  /* ---------------------------------------------------------- *
+   * Remove the final / from url, if there is one           *
+   * ---------------------------------------------------------- */
+    if (url[strlen(url) - 1] == '/')
+        url[strlen(url) - 1] = '\0';
+
+  /* ---------------------------------------------------------- *
+   * the first : ends the protocol string, i.e. http            *
+   * ---------------------------------------------------------- */
+    //strncpy(proto, url, (strchr(url, ':') - url));                  //proto 存 http/https (意義不明，待刪)
+
+  /* ---------------------------------------------------------- *
+   * the host_url starts after the "://" part                   *
+   * ---------------------------------------------------------- */
+    //strncpy(host_url, strstr(url, "://") + 3, sizeof(host_url));    //前端 :// 以前去掉，儲存在 host_url
+
+  /* ---------------------------------------------------------- *
+   * if the host_url contains a colon :, we got a port number   *
+   * ---------------------------------------------------------- */
+    if (strchr(host_url, ':'))
+    {
+        tmp_ptr = strchr(host_url, ':');
+        /* the last : starts the port number, if avail, i.e. 8443 */
+        strncpy(portnum, tmp_ptr + 1,  sizeof(portnum));
+        *tmp_ptr = '\0';
+    }
+
+    char* host_end = strchr(host_url, '/');
+    if(host_end)
+        *host_end = '\0';
+
+    port = atoi(portnum);
+
+    if ((host = gethostbyname(host_url)) == NULL)
+    {
+        printf("Error: Cannot resolve host_url %s.\n",  host_url);
+        abort();
+    }
+
+  /* ---------------------------------------------------------- *
+   * Print host_url iP address by Andric                        *
+   * ---------------------------------------------------------- */
+    //print_ip(host);
+  
+  /* ---------------------------------------------------------- *
+   * create the basic TCP socket                                *
+   * ---------------------------------------------------------- */
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    dest_addr.sin_family = AF_INET;
+    dest_addr.sin_port = htons(port);
+    dest_addr.sin_addr.s_addr = *(long*)(host->h_addr);
+
+  /* ---------------------------------------------------------- *
+   * Zeroing the rest of the struct                             *
+   * ---------------------------------------------------------- */
+    memset(&(dest_addr.sin_zero), '\0', 8);
+
+    tmp_ptr = inet_ntoa(dest_addr.sin_addr);
+
+  /* ---------------------------------------------------------- *
+   * Try to make the host connect here                          *
+   * ---------------------------------------------------------- */
+    if (connect(sockfd, (struct sockaddr *) &dest_addr, sizeof(struct sockaddr)) == -1)
+        printf("Error: Cannot connect to host %s [%s] on port %d.\n", host_url, tmp_ptr, port);
+
+    return sockfd;
+}
+
 
